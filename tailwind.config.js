32fd292dc3982ae97399e66eef65886d734fff2c
dreiesto <andreariestos@gmail.com>
2024-11-10 14:29:16 +0700

    dreiesto version 1.0
    Perbaikan font size pada header dengan custom font size

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       fontFamily:{
         'montserrat':["Montserrat","sans-serif"],
       },
+      fontSize: {
+        xxs: '0.625rem',
+      },
     },
   },
   plugins: [],
